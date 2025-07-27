da2629366fa0d16fc551a273a60b1ef59a50586f
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-27 10:02:24 +0600

    fg

--- welcome.php
+++ welcome.php
@@ -1,6 +1,13 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To My Site</title>
+</head>
+<body>
+    
 
 
-
-
-?>
+</body>
+</html>
