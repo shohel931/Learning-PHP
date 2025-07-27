d9633fb97e46df9b4c01e7ecbe6b1c1d311b1058
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-27 09:57:02 +0600

    ftg

--- test_form.php
+++ test_form.php
@@ -20,17 +20,24 @@
 
 <div class="form_area">
     <form action="welcome.php" method="POST">
-        Name: <input type="text" name="name" id="" placeholder="Name" required><br><br>
-        Father's Name: <input type="text" name="fname" id="" placeholder="Father's Name" required><br><br>
-        Mother's Name: <input type="text" name="name" id="" placeholder="Mother's Name" required><br><br>
-        Date of birth: <input type="number" name="dateof" id="" placeholder="31/01/2000" required> <br><br>
-        Phone: <input type="number" name="phone" id="" placeholder="Phone" required> <br><br>
-        Email: <input type="email" name="email" id="" placeholder="Email" required> <br><br>
+        Name: <br> </Name:br> <input type="text" name="name" id="" placeholder="Name" required><br><br>
+        Father's Name:<br> <input type="text" name="fname" id="" placeholder="Father's Name" required><br><br>
+        Mother's Name:<br> <input type="text" name="name" id="" placeholder="Mother's Name" required><br><br>
+        Date of birth:<br> <input type="number" name="dateof" id="" placeholder="31/01/2000" required> <br><br>
+        Phone:<br> <input type="number" name="phone" id="" placeholder="Phone" required> <br><br>
+        Email(optinal) :<br> <input type="email" name="email" id="" placeholder="Email" > <br><br>
 
     </form>
 </div>
 
-
+<style>
+    .form_area {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+</style>
 
 
     
