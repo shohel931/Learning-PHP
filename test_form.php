7506b3be513ca140eadfda86ea7cdc9c3a72e410
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-27 09:46:02 +0600

    rty

--- test_form.php
+++ test_form.php
@@ -20,7 +20,9 @@
 
 <div class="form_area">
     <form action="welcome.php" method="POST">
-        
+        Name: <input type="text" name="name" id="" placeholder="Name" required><br><br>
+        Email: <input type="email" name="email" id="" placeholder="Email" required> <br><br>
+        Phone: <input type="number" name="phone" id="" placeholder="Phone" required>
     </form>
 </div>
 
