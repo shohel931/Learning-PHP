553551fc86a5ef184eda1f93c89e68ef3f80e65b
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-27 09:39:10 +0600

    yg

---
+++ test_form.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Form</title>
+</head>
+<body>
+
+
+
+<div class="form_area">
+    <form action="welcome.php" method="POST">
+        
+    </form>
+</div>
+
+
+
+
+    
+</body>
+</html>
