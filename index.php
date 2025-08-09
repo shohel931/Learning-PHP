64e9d594cc83501bdea1334e1767ea5dea42fdc0
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-09 06:16:48 +0600

    k

--- index.php
+++ index.php
@@ -1,239 +1,69 @@
 <?php 
 
-$price = 56;
-$tax = 1.5;
-$total = $price + ($price * $tax);
+// Php Variable
 
-echo "Price: $price";
+$s = "Hello World!";
+echo $s;
+echo "<br>";
 
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
 
 echo "<br>";
 
 
-$name = "Shohel";
+// Global Variable
 
-$age = 18;
+$x = 5;
 
-echo "My name is $name and I am $age years old.";
+// function mytest() {
+    
+// }
 
+// mytest(); 
+echo $x;
 
 echo "<br>";
 
+$st = 10;
+$ts = 20;
 
-if ($age >= 18) {
-    echo "You are an adult.";
-} else {
-    echo "You are a minor.";
+function myTest() {
+    global $st, $ts;
+    $ts = $st + $ts;
 }
+myTest();
+echo $ts;
+
+
+echo "<br>";
+$g = "Ami Tomake Bhalobashi";
+
+echo str_replace("Tomake", "PHP Ke", $g);
 
 echo "<br>";
 
+$j = "Ami Tomake Bhalobashi";
+$k = explode(" ", $j);
+print_r($k);
 
-
-$txt = "Codding";
-echo "I love $txt";
-
-echo "<br>";
-// Concatenation
-echo " I love " . $txt . ".";
 
 echo "<br>";
 
-var_dump($txt);
+echo (round(0.60));
+echo "<br>";
+echo (round(0.50));
 echo "<br>";
 
-$name = "Shohel Rana";
-$age = 20;
-$height = 6.4;
-$weight = 55;
-
-echo "Name: $name";
-echo "<br>";
-echo "Age: $age";
-echo "<br>";
-echo "Height: "  . $height . " ft ";
-echo "<br>";
-echo "Weight: $weight kg";
+define("SHOHEL", "My Name is Shohel Rana.");
+echo SHOHEL;
 echo "<br>";
 
-var_dump($name);
-var_dump($age);
-var_dump($height);
-var_dump($weight);
-
-echo "<br>";
-
-$a = $b = $c = "Hello World";    
-echo "<br>";
-
-// Global variable
-$s = "Sohel Rana";
-function test() {
-    echo "This is a $s";
+define("RANA", "My Name is Shohel Rana.");
+function hello() {
+    echo RANA;
 }
-echo "This is a $s";
-echo "<br>";
-
-//  Local Scope variable
-
-function localTest() {
-    $local = "This is a local variable";
-    echo $local;
-}
-localTest();
-
-echo "<br>";
-
-$fname = "Sohel";
-$lname = "Rana";
-echo "My Name is " . $fname . " " . $lname;
-
-echo "<br>";
-echo "My Name is $fname $lname";
-
-
-echo "<br>";
-
-// if elese
-
-$email = "5";
-
-if (!empty($email)) {
-    echo "Email is set.";
-} else {
-    echo "Email is not set.";
-}
-
-echo "<br>";
-
-$d = date("H");
-
-if ($d < 10) {
-    echo "Good Morning";
-} else if ($d < 20) {
-    echo "Good Afternoon";
-} else{
-    echo "Good Evening";
-}
-
-
-echo "<br>";
-
-// short hand if else
-$t = 21;
-
-$s = ($t <= 18) ? "Hello" : "Good Bye.";
-
-echo $s ;
-echo "<br>";
-
-// Nested if Statement 
-
-if ($t > 10) {
-    echo "10 is over ";
-
-
-    if ($t > 20) {
-        echo "20 is over ";
-    } else {
-        echo " But 20 is not over";
-    }
-
-}
-echo "<br>";
-
-$marks = 32;
-
-if ($marks >= 33) {
-    echo "You are passed ";
-
-    if ($marks >= 80) {
-        echo " with A+";
-    } 
-} else {
-    echo "You are failed";
-}
-
-echo "<br>";
-
-
-
-$rolls = 1;
-
-
-switch ($rolls) {
-    case 1 :
-        echo "Your result is 4.20";
-        break;
-        
-    case 2 :
-        echo "Your result is 3.20";
-        break;
-        
-    case 3 :
-        echo "Your result is 4.00";
-        break;
-        
-    case 4 :
-        echo "Your result is 3.80";
-        break;
-        
-    case 5 :
-        echo "Your result is 4.40";
-        break;
-        
-    case 6 :
-        echo "Your result is 4.10";
-        break;
-        
-    case 7 :
-        echo "Your result is 4.90";
-        break;
-        
-    case 8 :
-        echo "Your result is 2.20";
-        break;
-        
-    case 9 :
-        echo "Your result is 3.52";
-        break;
-        
-    case 10 :
-        echo "Your result is 3.45";
-        break;
-        
-    case 11 :
-        echo "Your result is 3.75";
-        break;
-        
-    case 12 :
-        echo "Your result is 4.11";
-        break;
-        
-    case 13 :
-        echo "Your result is 4.20";
-        break;
-        
-    case 14 :
-        echo "Your result is 4.60";
-        break;
-        
-    case 15:
-        echo "Your result is 4.70";
-        break;
-     default:
-      echo "You are failed 123";
-        
-}
-
-
-
-
-
-
-
-
-
+hello();
 
 
 
@@ -242,27 +72,3 @@ switch ($rolls) {
 ?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style>
-
-    body {
-        background-color: #ddd;
-        color: #000;
-    }
-
-    
-</style>
